9e9311f15cdc0737fd78792c8a823ca7bce0dd5a
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2024-02-02 08:13:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
 
 const correctAns = num1 * num2;
 
+
 formEl.addEventListener("submit", () => {
   const userAns = +inputEl.value;
   if (userAns === correctAns) {
