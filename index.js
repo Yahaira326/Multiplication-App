ebfcb517d55fa77e697280740636fb856d947565
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2024-02-28 08:04:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ const num1 = Math.ceil(Math.random() * 10);
 const num2 = Math.ceil(Math.random() * 10);
 
 const questionEl = document.getElementById("question");
-
 const inputEl = document.getElementById("input");
-
 const formEl = document.getElementById("form");
-
 const scoreEl = document.getElementById("score");
-
 let score = JSON.parse(localStorage.getItem("score"));
 
 if (!score) {
